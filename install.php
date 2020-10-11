18babb55b727f51cb28bf74027ff14574dad2158
Patrick G. Leblanc <p.gleblanc@ti-ssi.com>
2020-10-11 12:41:13 +0000

    installer creating frontend

--- install.php
+++ install.php
@@ -1,5 +1,15 @@
 <?php
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+
+//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+echo PHP_VERSION;
+if (version_compare(PHP_VERSION, '7.3.0', '<')) {
+	echo 'Adamant require Php to be at least 7.3.0. you have  ' . PHP_VERSION . "\n";
+	exit();
+}
+
+
+
+
 $expected = file_get_contents("https://composer.github.io/installer.sig");
 
 copy('https://getcomposer.org/installer', 'composer-setup.php');
@@ -19,5 +29,7 @@ echo PHP_EOL;
 unlink('composer-setup.php');
 unlink('keys.dev.pub');
 unlink('keys.tags.pub');
+
+echo shell_exec('php composer.phar create-project ti-ssi/adamant-frontend frontend --repository-url ./package.json -s dev');
 
 echo 'Installation ended';
