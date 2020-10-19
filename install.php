5119ca4ac7845318f3535fe92d67b80512ea7729
Patrick G. Leblanc <p.gleblanc@ti-ssi.com>
2020-10-19 00:45:53 +0000

    first install file

--- install.php
+++ install.php
@@ -1,21 +1,19 @@
 <?php
 
 //ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-echo PHP_VERSION;
+
+//check requirements
 if (version_compare(PHP_VERSION, '7.3.0', '<')) {
 	echo 'Adamant require Php to be at least 7.3.0. you have  ' . PHP_VERSION . "\n";
 	exit();
 }
 
-
-
-
+//load and install composer
 $expected = file_get_contents("https://composer.github.io/installer.sig");
 
 copy('https://getcomposer.org/installer', 'composer-setup.php');
 
 $actual=hash_file('sha384', 'composer-setup.php');
-
 
 if ( $actual === $expected){ 
 	echo 'Installer verified';
@@ -30,6 +28,39 @@ unlink('composer-setup.php');
 unlink('keys.dev.pub');
 unlink('keys.tags.pub');
 
-echo shell_exec('php composer.phar create-project ti-ssi/adamant-frontend frontend --repository-url ./package.json -s dev');
+flush();
+
+//create required project
+echo shell_exec('php composer.phar create-project ti-ssi/adamant-frontend frontend --repository-url ./package-frontend.json -s dev --remove-vcs');
+flush();
+echo shell_exec('php composer.phar create-project ti-ssi/adamant-api api --repository-url ./package-api.json -s dev --remove-vcs');
+flush();
+
+//run npm
+echo shell_exec("npm --prefix ./frontend install ./frontend --cache ./");
+flush();
+echo shell_exec("npm --prefix ./frontend run dev --cache ./");
+flush();
+
+
+//write root htaccess file
+file_put_contents('.htaccess', "<IfModule mod_rewrite.c>
+	<IfModule mod_negotiation.c>
+		Options +FollowSymLinks -MultiViews -Indexes
+	</IfModule>
+	RewriteEngine On
+
+	RewriteRule ^api/(.*)$ /api/public/$1 [R=301,L]
+
+	RewriteRule ^admin/(.*)$ /admin/public/$1 [R=301,L]
+
+	RewriteCond %{REQUEST_URI} !^/frontend
+	RewriteCond %{REQUEST_URI} !^/api
+	RewriteCond %{REQUEST_URI} !^/admin
+	RewriteRule ^(.*)$ /frontend/public/$1 [L]
+</IfModule>
+
+");
 
 echo 'Installation ended';
+
